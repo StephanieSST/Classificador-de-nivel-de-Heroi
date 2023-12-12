8145dbe0c3c949b660a8af3768bdbc3f381215d6
StephanieSST <stephaniesteuernagel@gmail.com>
2023-12-12 01:30:01 -0300

    OiE

--- pratica.js
+++ pratica.js
@@ -8,3 +8,45 @@ pontosDeVida += 1
 
   console.log(pontosDeVida + " Totais.")
   
+
+  // switch (xp) {
+
+//     case 1000:
+//         alert("Ferro");
+//         break;
+    
+//         case (xp > 1000 && xp <= 2000):
+//         nivel = "Bronze"
+//         break;
+
+//         case (xp > 2000 && xp <= 5000):
+//             nivel = "Prata"
+//             break;
+
+//             case (xp > 6000 && xp <= 7000):
+//             nivel = "Ouro"
+//             break;
+
+//             case (xp > 7000 && xp <= 8000):
+//                 nivel = "Platina"
+//                 break;
+
+//                 case (xp > 8000 && xp <= 9000):
+//                     nivel = "Ascendente"
+//                     break;
+
+//                     case (xp > 9000 && xp <= 10000):
+//                         nivel = "Imortal"
+//                         break;
+
+//                         case (xp >= 10001):
+//                             nivel = "Radiante"
+//                             break
+                            
+//                             default:
+//                                 alert("Erro! Experiência Inválida!")
+        
+
+// }
+// alert("O Herói de nome " + nome + " está no nível de " + nivel) 
+
